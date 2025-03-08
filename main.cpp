ea4ca65995d541f3533c602c5ac5a5130348f7ff
Jerelynm <jmarinm4@miumg.edu.gt>
2025-03-07 20:44:47 -0600

    Ejercicio 26

--- main.cpp
+++ main.cpp
@@ -382,3 +382,24 @@ void ejercicioFOR5() {
 
     cout << endl; // Salto de línea al final
 }
+void ejercicioFOR6() {
+    int z;
+
+    // Solicitar al usuario que ingrese un número entero positivo
+    cout << "Ingresa un número entero positivo: ";
+    cin >> z;
+
+    // Inicializar la variable para la suma de los números triangulares
+    int suma = 0;
+
+    cout << "La serie de números triangulares hasta " << z << " es:" << endl;
+
+    // Bucle for para calcular e imprimir la serie de números triangulares
+    for (int i = 1; suma <= z; i++) {
+        suma += i; // Sumar el número actual
+        // Imprimir el número triangular actual solo si es menor o igual a n
+        cout << suma * (suma <= z) << " "; // Multiplicamos por 1 si suma es menor o igual a n, de lo contrario por 0
+    }
+
+    cout << endl; // Salto de línea al final
+}

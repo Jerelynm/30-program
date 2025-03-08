2bbb6713b1e75a671018e75289d8362a0b639690
Jerelynm <jmarinm4@miumg.edu.gt>
2025-03-07 19:28:15 -0600

    Ejercicio 20

--- main.cpp
+++ main.cpp
@@ -281,3 +281,15 @@ void ejwh9() {
         i++; // Incrementamos el número natural
     }
 }
+void ejwh10() {
+    int i = 1; // Comenzamos desde el primer número natural
+    int producto = 1; // Inicializamos el producto en 1
+
+
+    while (i <= 10) {
+        producto *= i; // Multiplicamos el producto por el número actual
+        i++; // Incrementamos el número natural
+    }
+
+    cout << "El producto de los primeros 10 numeros naturales es: " << producto << endl;
+}

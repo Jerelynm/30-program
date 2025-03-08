894849954781db6e3ce2a85dd9b3d8a850a97be9
Jerelynm <jmarinm4@miumg.edu.gt>
2025-03-07 18:18:55 -0600

    Ejercicio 6

--- main.cpp
+++ main.cpp
@@ -73,3 +73,15 @@ void ejercicioif5() {
         cout << "La letra "<< letter <<" es una consonante"<<endl;
     }
 }
+void ejercicioif6() {
+    int cm;
+
+    cout << "Ingrese el numero que desea: "<<endl;
+    cin >> cm;
+
+    if (cm % 2 == 0) {
+        cout << "El numero " <<cm<< " no es primo"<<endl;
+    } else {
+        cout << "El numero " <<cm<< " es primo"<<endl;
+    }
+}

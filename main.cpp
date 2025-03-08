a5a448cd333a94275ef8a6d2e35a0332226b105f
Jerelynm <jmarinm4@miumg.edu.gt>
2025-03-07 18:57:34 -0600

    Ejercicio 10

--- main.cpp
+++ main.cpp
@@ -135,3 +135,24 @@ void ejercicioif9() {
         cout <<"Calificacion no valida, debe estar entre 0 y 100"<<endl;
     }
 }
+void ejercicioif10() {
+    double precio0, PrecioDesc, PrecioFin;
+
+    cout<<"Determine el precio con el descuento"<<endl;
+    cout<<"Ingrese el precio: "<<endl;
+    cin>>precio0;
+
+    cout<<"Ingrese el descuento: "<<endl;
+    cin>>PrecioDesc;
+
+    if (precio0 < 0 or PrecioDesc < 0) {
+        cout <<"No pueden ser numeros negativos"<<endl;
+    } else if (PrecioDesc > 100) {
+        cout <<"El descuento no puede ser mayor a 100%"<<endl;
+    } else if (PrecioDesc == 0) {
+        cout <<"No hay descuento "<<precio0<<" es el precio"<<endl;
+    } else {
+        PrecioFin = precio0-(precio0*(PrecioDesc/100));
+        cout<<"El precio con descuento es:Q "<<PrecioFin<<endl;
+    }
+}

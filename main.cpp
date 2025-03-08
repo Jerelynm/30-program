83175773fa9e2bb032f15023039672b033c2c9a9
Jerelynm <jmarinm4@miumg.edu.gt>
2025-03-07 20:59:17 -0600

    Ejercicio 29

--- main.cpp
+++ main.cpp
@@ -431,3 +431,27 @@ void ejercicioFOR8() {
     // Imprimir el resultado
     cout << "La suma de los primeros 100 numeros naturales es: " << suma << endl;
 }
+int ejercicioFOR9() {
+    int aj;
+
+    // Solicitar al usuario que ingrese un número entero
+    cout << "Ingresa un numero entero positivo: ";
+    cin >> aj;
+
+    // Verificar que el número ingresado sea positivo
+    if (aj <= 0) {
+        cout << "Por favor, ingresa un numero entero positivo." << endl;
+        return 1; // Salir del programa si el número no es positivo
+    }
+
+    cout << "Los divisores de " << aj << " son:" << endl;
+
+    // Bucle for para encontrar e imprimir los divisores del número
+    for (int i = 1; i <= aj; i++) {
+        if (aj % i == 0) { // Verificar si i es un divisor de numero
+            cout << i << " "; // Imprimir el divisor
+        }
+    }
+
+    cout << endl; // Salto de línea al final
+}

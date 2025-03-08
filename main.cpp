a8fc35d3f363a2b827e25061fc0a35c6aba319ae
Jerelynm <jmarinm4@miumg.edu.gt>
2025-03-07 19:23:16 -0600

    Ejercicio 18

--- main.cpp
+++ main.cpp
@@ -257,3 +257,16 @@ void ejwh7() {
 
     cout << "La media de los numeros ingresados es: " << media << endl;
 }
+void ejwh8() {
+    int Y;
+
+    cout << "Ingresa numeros (ingresa 0 para terminar):" << endl;
+    cin >> Y; // Leer el primer número antes del bucle
+
+    while (Y != 0) {
+        cout << "Ingresa otro numero: " << endl;
+
+        // Leer el siguiente número
+        cin >> Y;
+    }
+}

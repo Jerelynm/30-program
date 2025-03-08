1668574256577081a1cf29bf362403863084c024
Jerelynm <jmarinm4@miumg.edu.gt>
2025-03-07 19:14:06 -0600

    Ejercicio 15

--- main.cpp
+++ main.cpp
@@ -209,3 +209,20 @@ void ejwh4() {
     }
     cout<<"\n";
 }
+void ejwh5() {
+    int rz, suma=0;
+
+    cout<<"Ingrese un numero positivo: "<<endl;
+    cin>>rz;
+
+    while (rz>=0) {
+        if (suma + rz > 100) {
+            cout << "La suma ha alcanzado el limite de 100." << endl;
+            break; // Salir porque alcanzo el limite
+        }
+        suma+=rz;
+        cout<<"Ingrese otro numero: "<<endl;
+        cin>>rz;
+    }
+    cout<<"La suma es: "<<suma<<endl;
+}
